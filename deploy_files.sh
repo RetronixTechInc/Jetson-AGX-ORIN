526731d400cbc1e89f816c889e66dbcee9700ea8
ArtieWang <artiewang@retronix.com.tw>
2026-01-07 16:24:32 +0800

    modify deploy_files.sh

--- deploy_files.sh
+++ deploy_files.sh
@@ -2,7 +2,7 @@
 
 TARGET_PATH=$1
 ACTION=$2
-FILE_LIST=$(find . \( -name ".git" -o -name "deploy.sh" \) -prune -o -type f -print)
+FILE_LIST=$(find . \( -name ".git" -o -name "deploy_files.sh" \) -prune -o -type f -print)
 
 print_usage() {
 	echo "deploy.sh target_path diff : list files which are different with target"
